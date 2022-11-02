13f10ccc7d3788c56e3758e1568d5fdf8ada98e9
shivanichouhan21 <prashant.p@cisinlabs.com>
2022-11-02 17:46:45 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const ronin = require('ronin-server')
+const mocks = require('ronin-mocks')
+const database  = require( 'ronin-database' )
+
+async function main() {
+
+    try {
+    await database.connect("mongodb://172.17.0.1:27017/notes" )
+    
+    const server = ronin.server({
+            port: process.env.SERVER_PORT
+        })
+        server.use( '/foo', (req, res) => {
+            return res.json({ "foo": "bar" })
+        })
+        server.use( '/', mocks.server( server.Router()) )
+
+    const result = await server.start()
+        console.info( result )
+    
+    } catch( error ) {
+        console.error( error )
+    }
+}
+main()
---
+++ test/test.js
@@ -0,0 +1,15 @@
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
+
+
+// dckr_pat_fvbOscrZVYmeh6lqRIzVoM2w0mk
+
+
+
+//dckr_pat_ze6P4-tcXYFs5DkYIR249g_uXrc
